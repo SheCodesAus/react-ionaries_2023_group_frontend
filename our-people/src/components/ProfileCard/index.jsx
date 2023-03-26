5e79666346bbe6370e20c660c60f45c3d70148cc
AnnieJDesigns <114784081+AnnieJDesigns@users.noreply.github.com>
2023-03-26 18:40:09 +1100

    Added in Profile Card

---
+++ our-people/src/components/ProfileCard/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './ProjectCard.css'
+
+<div className="project-card">
+    <img src="../assets/hetal-p.jpg"></img>
+    <h3>Name</h3>
+    <h5>Pronoun</h5>
+    <h4>New Job</h4>
+    <h4>Old Job</h4>
+</div>
+
+export default ProjectCard;
+
--- our-people/src/pages/HomePage.jsx
+++ our-people/src/pages/HomePage.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import ProjectCard  from "../components/ProfileCard";
+
 
 function HomePage() {
-    return <h1>This is the home page.</h1>;
+    return (
+        <body>
+        <h1>This is the home page.</h1>
+        <div><ProjectCard/></div>
+        </body>
+
+    ) 
 }
 export default HomePage;
