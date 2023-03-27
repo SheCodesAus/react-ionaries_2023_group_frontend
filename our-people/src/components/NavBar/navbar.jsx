ed4810d29417a0bfa0e53ce18f29cb33cb691ffd
AnnieJDesigns <114784081+AnnieJDesigns@users.noreply.github.com>
2023-03-27 18:57:25 +1100

    Corrected navbar name

--- our-people/src/components/NavBar/navbar.jsx
+++ our-people/src/components/NavBar/navbar.jsx
@@ -8,8 +8,8 @@ function Nav() {
             <Link to="/">Home</Link>
             <Link to="/signup">Sign up</Link>
             <Link to="/createprofile">Create Profile</Link>
-            <Link to="/profileid">Profile ID</Link>
-            <Link to="/allstories">All Stories</Link>
+            <Link to="/profileid">My Profile</Link>
+            <Link to="/allstories">View Mentors</Link>
         </nav>
     )
 }
