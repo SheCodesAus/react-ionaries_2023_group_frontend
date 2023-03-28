8288648ba561bd65fb9ee5d54f0e4168684e9cda
Julia Moskvita <ymoskvita@gmail.com>
2023-03-28 19:17:03 +1100

    fix

--- our-people/src/components/ProfileCard/index.jsx
+++ our-people/src/components/ProfileCard/index.jsx
@@ -3,10 +3,11 @@ import {Link} from 'react-router-dom';
 import './ProfileCard.css'
 
 function ProfileCard(props) {
+    const { profileData } = props;
 return (
     <div className="profile-card">
         <Link to={`/profile/${profileData.id}`}>
-            <img src={profileData.image}></img>
+            <img src={profileData.profile_imag}></img>
             <h3>{profileData.first_name}{profileData.last_name}</h3>
             <h4>{profileData.pronouns}</h4>
             <h4>{profileData.current_role}</h4>
--- our-people/src/pages/HomePage.jsx
+++ our-people/src/pages/HomePage.jsx
@@ -3,21 +3,60 @@ import ProfileCard from "../components/ProfileCard";
 
 
 function HomePage() {
-    const [profileData, setProfileData] = useState([]);
+    // const [profileData, setProfileData] = useState([]);
 
-    useEffect(() => {
-        fetch(`${import.meta.env.VITE_API_URL} profiles/${id}`)
-        .then((profileResults) => {
-            return profileResults.json();
-        })
-        .then((data) => {
-            const recentProfiles = data
-            .sort((a,b) => new Date(b.date_created) - new Date(a.date_created))
-            .slice(0,3);
-            setProjectList(recentProfiles);
-        });
-    }, []);
-
+    // useEffect(() => {
+    //     fetch(`${import.meta.env.VITE_API_URL}profiles/${id}`)
+    //     .then((profileResults) => {
+    //         return profileResults.json();
+    //     })
+    //     .then((data) => {
+    //         const recentProfiles = data
+    //         .sort((a,b) => new Date(b.date_created) - new Date(a.date_created))
+    //         .slice(0,3);
+    //         setProjectList(recentProfiles);
+    //     });
+    // }, []);
+    const profileData = [
+        {
+            "id": 1,
+            "display_name": "John",
+            "bio": "my story...",
+            "previous_role": "role1",
+            "current_role": "role2.",
+            "profile_image": "https://via.placeholder.com/300.jpg",
+            "birthdate": "2023-03-19",
+            "pronouns": "This field is required.",
+            "gender": "This field is required.",
+            "ethnicity": "This field is required.",
+            "linkedin_url": "https://www.linkedin.com/",
+            "github_url": "https://www.linkedin.com/",
+            "is_visible": true,
+            "is_public": true,
+            "allow_contact": true,
+            "challenge": "This field is required.",
+            "user_id": 1
+        },
+        {
+            "id": 2,
+            "display_name": "John",
+            "bio": "my story...",
+            "previous_role": "role1",
+            "current_role": "role2.",
+            "profile_image": "https://via.placeholder.com/300.jpg",
+            "birthdate": "2023-03-19",
+            "pronouns": "This field is required.",
+            "gender": "This field is required.",
+            "ethnicity": "This field is required.",
+            "linkedin_url": "https://www.linkedin.com/",
+            "github_url": "https://www.linkedin.com/",
+            "is_visible": true,
+            "is_public": true,
+            "allow_contact": true,
+            "challenge": "This field is required.",
+            "user_id": 1
+        }
+    ];
 
         return (
             <div>
@@ -25,7 +64,7 @@ function HomePage() {
                     <div className = "profile-wrapper">
                         <div className = "container">
                             {profileData.map((profileData, key)=> {
-                                    return <ProfileCard key={key} profileData={profileData} />;
+                                return <ProfileCard key={key} profileData={profileData} />;
                                 }
                             )}
                         </div>
