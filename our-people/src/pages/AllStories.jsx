f01ef80816b0a12ccfa4e34518c2cb72591db94d
AnnieJDesigns <114784081+AnnieJDesigns@users.noreply.github.com>
2023-04-02 21:13:45 +1000

    Removed headings in allstories.jsx in order to not conflict with main

--- our-people/src/pages/AllStories.jsx
+++ our-people/src/pages/AllStories.jsx
@@ -16,8 +16,6 @@ function AllStoriesPage() {
     
         return (
             <div>
-                <h1>All Stories</h1>
-                <h3>Be inspired</h3>
                 <div>
                     {profileList.map((profileData, key)=> {
                         return <ProfileCard key={key} profileData={profileData} />;
