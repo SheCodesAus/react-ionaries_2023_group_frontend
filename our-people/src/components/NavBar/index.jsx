d082fb2979f070da8e6598c93579f9bb238feddb
Julia Moskvita <ymoskvita@gmail.com>
2023-04-04 19:30:08 +1000

    fix avatar link

--- our-people/src/components/NavBar/index.jsx
+++ our-people/src/components/NavBar/index.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import './nav.css'
 
 function Nav() {
+    const [profileData, setProfileData] = useState([]);
+
     const isLogIn = window.localStorage.getItem("token") !== null;
     const username = window.localStorage.getItem("username") || "";
     const navigate = useNavigate();
 
+    useEffect(() => {
+            fetch(`${import.meta.env.VITE_API_URL}users/`)
+            .then((profileResults) => {
+                return profileResults.json();
+            })
+            .then((data) => {
+                const userProfile = data.find((profile)=>profile.username===username)
+                setProfileData(userProfile);
+                console.log(userProfile);
+            });
+    }, []);
 
     const onLogOut = () => {
         localStorage.clear();
@@ -18,8 +31,8 @@ function Nav() {
         <header>
             <Link to="/"><img src={logo} alt="logo" /></Link>
             <nav>
-                {isLogIn && <Link className='user-icon' to="/profile/:id">{username.slice(0,1)}</Link>}
-                {isLogIn && <Link to="/createprofile">Create Profile</Link>}
+                {isLogIn && <Link className='user-icon' to={`/profile/${profileData.id}`}>{`${profileData.first_name.slice(0,1)}${profileData.last_name.slice(0,1)}`}</Link>}
+                {isLogIn && <Link to="/create-profile">Create Profile</Link>}
                 {isLogIn && <Link to="/allstories">All Stories</Link>}
                 {isLogIn && <Link className='secondary-btn' to="/" onClick={onLogOut}>Log out</Link>}
                 {!isLogIn && <Link to="/login">Log in</Link>}
